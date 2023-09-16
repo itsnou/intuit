4e1b8f4aaff2c25ed948c85c5fb334c0cc9b0973
Emiliano <emiliano.meneses@gmail.com>
2023-09-16 11:32:17 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title> Challange Intuit </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
